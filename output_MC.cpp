ac1aa604cd0b837e51344b53f959f9f55af25e81
JStewart18 <austin95@uw.edu>
2017-10-24 01:08:42 -0700

    output_MC.cpp
    Replaced improper syntax previously used as an outline with code. SearchDown now properly refers to each child node.

--- output_MC.cpp
+++ output_MC.cpp
@@ -5,11 +5,11 @@
 class Output_MC {
 	//Algorithm 6
 
-	std::vector<boost::property_tree::ptree> output_MC(boost::property_tree::ptree x, CliqueTree tree){
+	std::vector<boost::property_tree::ptree> output_MC(boost::property_tree::ptree x, CliqueTree* tree){
 		std::vector<boost::property_tree::ptree> C;
 
-		C[x.level] = x.data;		//x.key won't work
-		if () {	//if x is an element of t.leaflist
+		C[tree->level(x.key)] = x.data;		
+		if (tree->leafList.contains(x)) {	//if x is an element of t.leaflist
 			searchUp(C, x, tree);
 		} else {
 			searchUp(C, x, tree);
@@ -19,21 +19,21 @@ class Output_MC {
 	}
 };
 				
-void searchUp(std::vector<boost::property_tree::ptree> clique, boost::property_tree::ptree x, CliqueTree overallTree) {//CliqueTree = clique subtree, T = overall root tree
+void searchUp(std::vector<boost::property_tree::ptree> clique, boost::property_tree::ptree x, CliqueTree* overallTree) {//CliqueTree = clique subtree, T = overall root tree
 
-	while (x.data == overallTree.topRoot.data) {
-		x = x.forcepath(x.key);
-		clique.[level.x] = x.label;
+	while (x.data != overallTree->topRoot.data) {
+		//x = overallTree->parentOf(x);
+		clique[overallTree->level(overallTree->parentOf(x).get_child(x.key).first)] = x.data; //check the keys of the fellow children to original x's parent node
 	}
 }
-boost::property_tree::ptree searchDown(std::vector<boost::property_tree::ptree> clique, boost::property_tree::ptree x, CliqueTree overallTree) {//c=current clique, cliques = subset of tree
-	if (x is leaf in T) {
-		return clique[i]: 1 <= i <= level(x)//} as an MC
-	}
-	else {
-		for (auto __begin = x.children.begin, __end = x.children.end; __begin != __end; ++__begin) {
-			x.children[__begin] = y.label;
-			return searchDown(clique, y, overallTree);
+std::vector<boost::property_tree::ptree> searchDown(std::vector<boost::property_tree::ptree> clique, boost::property_tree::ptree x, CliqueTree* overallTree) {//c=current clique, cliques = subset of tree
+	if (overallTree->leafList.contains(x)) {//x is a leaf of t
+		return clique;						//} as an MC
+	} else {
+
+		for(auto const &y: x.get_child(x.key)) {					// children y of ptree x
+			clique[overallTree->level(y.first)] = y.second;			//y.label to store at C[y.level]
+			return searchDown(clique, y.second, overallTree);		//need to confirm y.second is correct for the ptree child for both of these <^
 		}
 	}
 }
